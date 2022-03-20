a2a7823f6e786b0c4362868f7c128e3b08f56485
Hyeong Suk <hyngsk.o@gmail.com>
2022-03-20 17:51:20 +0900

    Initial commit

---
+++ src/main/java/works/hyngsxk/expot/ExpotApplication.java
@@ -0,0 +1,13 @@
+package works.hyngsxk.expot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/works/hyngsxk/expot/ExpotApplicationTests.java
@@ -0,0 +1,13 @@
+package works.hyngsxk.expot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
